af7e9c4e64690ee135fb4f4a8b9cc9d49311e50d
megha-cloud <56022420+megha-cloud@users.noreply.github.com>
2019-10-22 21:36:14 +0530

    Update DockerPracticeHelperTest.java

--- src/test/java/com/devops/practice/DockerPracticeHelperTest.java
+++ src/test/java/com/devops/practice/DockerPracticeHelperTest.java
@@ -1,1 +1,35 @@
 
+package com.docker.practice.test;
+
+import org.junit.Test;
+
+import com.docker.practice.DockerPracticeHelper;
+
+public class DockerPracticeHelperTest {
+
+	DockerPracticeHelper helper = new DockerPracticeHelper();
+	
+	@Test
+	public void addTest(){
+		
+		helper.add();	
+	}
+	
+	@Test
+	public void subtractTest(){
+		
+		helper.subtract();	
+	}
+	
+	@Test
+	public void multiplyTest(){
+		
+		helper.multiply();	
+	}
+	
+	@Test
+	public void divideTest(){
+		
+		helper.divide();	
+	}
+}
